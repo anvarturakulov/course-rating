27fd0cee9ea77afdb73f793cbc5dc61d9355f438
anvarturakulov <anvarsam@mail.ru>
2023-08-29 18:05:02 +0500

    start components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css';
+// import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+// import Head from 'next/head';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) : JSX.Element{
-  return <Component {...pageProps} />;
+  return  <>
+    
+    <Component {...pageProps} />
+  </>;
 }

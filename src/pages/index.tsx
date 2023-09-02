975ab8bcc8bd39f1644ea0e52a06d0b0aefb7420
anvarturakulov <anvarsam@mail.ru>
2023-09-02 20:18:55 +0500

    footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { Button, Htag, P, Tag, Rating } from "@/components";
-import { Layout } from "@/layout/Layout";
+import React, {useState } from "react";
+import { P, Tag, Rating } from "@/components";
+import { withLayout } from "@/layout/Layout";
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   
   const [rating, setRating] = useState<number>(2);
 
   return (
-    <Layout>
+    <>
       <P size="m">Выше указаны программы Adobe InDesign, Adobe Illustrator, Corel Draw и ими можно успешно пользоваться дома или в дороге. Современные ноутбуки хорошо справляются с нагрузкой, так зачем загонять специалиста в душный офис. В этой профессии важным считается вдохновение, поэтому дизайнеры ищут его в разных местах.</P>
       <Tag color="ghost">Ghost</Tag>
       <Tag color="red">Red</Tag>
@@ -15,6 +15,8 @@ export default function Home(): JSX.Element {
       <Tag color="green">Green</Tag>
       <Tag color="primary" size="m" href='https://google.com'>Primary</Tag>
       <Rating rating={rating} isEditable={true} setRating={setRating}/>
-    </Layout>
+    </>
   );
 }
+
+export default withLayout(Home);

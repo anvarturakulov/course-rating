6f952bf1fc8a1b8ed1e2c9a465b7eaa57a3bfa0e
anvarturakulov <anvarsam@mail.ru>
2023-09-05 16:13:18 +0500

    added Link for menu items

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 import { FooterProps } from "./Footer.props";
 import styles from './Footer.module.css';
 import cn from 'classnames';
-import { P } from "@/components";
 import {format} from 'date-fns';
 
 export const Footer = ({className, ... props }: FooterProps): JSX.Element => {
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,10 @@
 import { LayoutProps } from "./Layout.props";
 import styles from './Layout.module.css';
-import cn from 'classnames';
 import { Header } from "./Header/Header";
 import { Footer } from "./Footer/Footer";
 import { Sidebar } from "./Sidebar/Sidebar";
 import {FunctionComponent } from "react";
+import { AppContextProvider, IAppContext } from "@/context/app.context";
 
 const Layout = ({children }: LayoutProps): JSX.Element => {
     return (  
@@ -19,12 +19,15 @@ const Layout = ({children }: LayoutProps): JSX.Element => {
     );
 };
 
-export const withLayout = <T extends Record <string, undefined>>(Component : FunctionComponent<T>) => {
+export const withLayout = <T extends Record <string, undefined> & IAppContext>(Component : FunctionComponent<T>) => {
     return function withLayoutComponent( props : T): JSX.Element {
         return (
-            <Layout>
-                <Component {...props}/>
-            </Layout>
+            <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+                <Layout>
+                    <Component {...props} />
+                </Layout>
+            </AppContextProvider>
+            
         );
     };
 };
